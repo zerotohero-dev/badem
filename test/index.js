72906aebcbaace99fd1718a927ff2e25bee8efc6
Volkan Özçelik <volkan.ozcelik@gmail.com>
2017-06-29 12:11:12 -0700

    Update index.js

--- test/index.js
+++ test/index.js
@@ -6,11 +6,7 @@
  *
  * This project is a part of the “Byte-Sized JavaScript” videocast.
  *
-<<<<<<< HEAD
- * You can watch “Byte-Sized JavaScript” at: https://bit.ly/bytesized
-=======
  * You can watch “Byte-Sized JavaScript” at: https://bytesized.tv/
->>>>>>> dea3d45e14d9721fcd3ce576851a8b88b8d7c5f8
  *
  * MIT Licensed — See LICENSE.md
  *
